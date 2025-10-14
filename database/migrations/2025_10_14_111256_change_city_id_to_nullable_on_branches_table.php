688981c2e5fb978c4ee08cec021552bc6450b9b9
youssef <youssef@example.com>
2025-10-14 13:23:10 +0200

    Fix: Dashboard + Branches

---
+++ database/migrations/2025_10_14_111256_change_city_id_to_nullable_on_branches_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('branches', function (Blueprint $table) {
+            // Make city_id nullable
+            $table->unsignedBigInteger('city_id')->nullable()->change();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('branches', function (Blueprint $table) {
+            // Revert city_id to not nullable
+            $table->unsignedBigInteger('city_id')->nullable(false)->change();
+        });
+    }
+};
